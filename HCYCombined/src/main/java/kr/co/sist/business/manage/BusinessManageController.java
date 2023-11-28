b3f5c812f5d82c280eb850b274c679579ac6b6a1
user <user@DESKTOP-COJ9858>
2023-11-28 15:47:52 +0900

    선빵

---
+++ HCYCombined/src/main/java/kr/co/sist/business/manage/BusinessManageController.java
@@ -0,0 +1,21 @@
+package kr.co.sist.business.manage;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BusinessManageController {
+	
+	@GetMapping("/BusinessManage/businessMain.do")
+	public String goBookingManage(HttpSession session, Model model) {
+		session.setAttribute("code", "»ç¾÷ÀÚ");
+		
+		return "BusinessManage/businessmanage_hotel.jsp";
+	}//goBookingManage
+	public String cancelBooking(HttpSession session,String hotelCode) {
+		return "";
+	}
+}//class
